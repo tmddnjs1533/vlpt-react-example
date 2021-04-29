d5f7f3a85d4ab9fd0e642c3d3dd45b8d078f9058
Kim Seoungwon <sw91@humanf.co.kr>
2021-04-29 18:53:03 +0900

    Custom Hooks with useReducer - input

--- src/hooks/useInputs.js
+++ src/hooks/useInputs.js
@@ -1,14 +1,32 @@
-import {useCallback, useState} from "react";
+import {useCallback, useReducer } from "react";
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                [action.name]: action.value
+            }
+        case 'RESET':
+            return Object.keys(state).reduce((acc,current) => {
+                acc[current] = '';
+                return acc;
+            }, {})
+        default:
+            return state;
+    }
+}
 
 function useInputs(initialForm) {
-    const [form, setForm] = useState(initialForm)
+    const [form, dispatch] = useReducer(reducer, initialForm)
 
     // change
     const onChange = useCallback( e => {
         const { name, value } = e.target;
-        setForm(form => ({ ...form, [name]: value }))
+        // setForm(form => ({ ...form, [name]: value }))
+        dispatch({ type: 'CHANGE', name, value })
     }, [])
-    const reset = useCallback(() => setForm(initialForm), [initialForm])
+    const reset = useCallback(() => dispatch({ type: 'RESET'}), [])
     return [form, onChange, reset]
 }
 
