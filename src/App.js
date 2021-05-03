29a61fd3a563c9fdabee51f5a34b98b2103b14df
Kim Seoungwon <sw91@humanf.co.kr>
2021-05-03 09:05:09 +0900

    immer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useReducer } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
+import produce from "immer";
 
 function countActiveUsers(users) {
     console.log('활성 사용자 수를 세는 중...')
@@ -31,21 +32,19 @@ const initialState = {
 function reducer(state, action) {
     switch (action.type) {
         case 'CREATE_USER':
-            return {
-                users: state.users.concat(action.user)
-            }
+            return produce(state, draft => {
+                draft.users.push(action.user)
+            })
         case 'TOGGLE_USER':
-            return {
-                ...state,
-                users: state.users.map(user =>
-                    user.id === action.id ? { ...user, active: !user.active } : user
-                )
-            }
+            return produce(state, draft => {
+                const user = draft.users.find(user => user.id === action.id)
+                user.active = !user.active
+            })
         case 'REMOVE_USER':
-            return {
-                ...state,
-                users: state.users.filter(user => user.id !== action.id)
-            }
+            return produce(state, draft => {
+                const index = draft.users.findIndex(user => user.id === action.id)
+                draft.users.splice(index, 1)
+            })
         default:
             return state;
     }
@@ -56,13 +55,7 @@ export const UserDispatch = React.createContext(null)
 function App() {
 
     const [state, dispatch] = useReducer(reducer, initialState)
-
-
     const { users } = state;
-
-
-
-
 
     const count = useMemo(() => countActiveUsers(users), [users])
 
